779a1faf16e9178d2df8337d622630c57549b6ff
mpottebaum <mpottebaum@gmail.com>
2021-01-21 21:38:12 -0600

    CRUD routes for StoryNodes controller w/ nested choices

--- app/controllers/story_nodes_controller.rb
+++ app/controllers/story_nodes_controller.rb
@@ -1,13 +1,40 @@
 class StoryNodesController < ApplicationController
     def create
+        story_node = StoryNode.create(story_node_params)
+
+        render json: story_node
     end
 
     def show
-    end
+        story_node = StoryNode.find(params[:id])
 
+        render json: story_node, include: [ :choices ]
+    end
+    
     def update
+        story_node = StoryNode.find(params[:id])
+        story_node.update(story_node_params)
+
+        render json: story_node, include: [ :choices ]
     end
 
     def destroy
+        story_node = StoryNode.find(params[:id])
+        story_node.destroy
+
+        render json: { message: 'success' }
+    end
+
+    private
+
+    def story_node_params
+        params.require(:story_node)
+        .permit(
+            :name,
+            :content,
+            :next_node_id,
+            :story_id,
+            :choices_attributes => [ :content, :next_node_id ]
+        )
     end
 end
--- app/models/story_node.rb
+++ app/models/story_node.rb
@@ -1,4 +1,5 @@
 class StoryNode < ApplicationRecord
     belongs_to :story
     has_many :choices
+    accepts_nested_attributes_for :choices
 end
