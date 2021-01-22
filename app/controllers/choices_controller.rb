0d17599fd36fb3465e49312066887c495b37dc2e
mpottebaum <mpottebaum@gmail.com>
2021-01-21 21:49:13 -0600

    show, update, destroy choices controller

--- app/controllers/choices_controller.rb
+++ app/controllers/choices_controller.rb
@@ -1,4 +1,27 @@
 class ChoicesController < ApplicationController
+    def show
+        choice = Choice.find(params[:id])
+
+        render json: choice
+    end
+
+    def update
+        choice = Choice.find(params[:id])
+        choice.update(choices_params)
+
+        render json: choice
+    end
+
     def destroy
+        choice = Choice.find(params[:id])
+        choice.destroy
+
+        render json: { message: 'success' }
+    end
+
+    private
+
+    def choices_params
+        params.require(:choice).permit(:content, :next_id)
     end
 end
