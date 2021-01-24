499f5a1a7873cad676bbf164def028ccc9c11dca
mpottebaum <mpottebaum@gmail.com>
2021-01-23 18:16:07 -0600

    added cannot_have_both_next_node_id_and_choices validation to StoryNode

--- app/models/story_node.rb
+++ app/models/story_node.rb
@@ -4,4 +4,13 @@ class StoryNode < ApplicationRecord
     accepts_nested_attributes_for :choices
 
     validates :content, presence: true
+    validate :cannot_have_both_next_node_id_and_choices
+
+    private
+
+    def cannot_have_both_next_node_id_and_choices
+        if next_node_id.present? && choices.length > 0
+            errors.add(:next_node_id, "Node cannot have both a next_node_id and choices")
+        end
+    end
 end
