eab4c4af5b743cd4881e3462ee2f5306a7476397
mpottebaum <mpottebaum@gmail.com>
2021-02-03 20:24:43 -0600

    added stories#index endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  get '/stories', to: 'stories#index'
   post '/stories', to: 'stories#create'
   get '/stories/:id', to: 'stories#show'
   put '/stories/:id', to: 'stories#update'
