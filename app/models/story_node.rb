37234f8efe9cca512eb9d54c9c4819ab4de02fc9
mpottebaum <mpottebaum@gmail.com>
2021-02-18 15:40:08 -0600

    validate choice coordinates are unique + updated story_node grid validation messages

--- app/models/story_node.rb
+++ app/models/story_node.rb
@@ -4,7 +4,7 @@ class StoryNode < ApplicationRecord
     accepts_nested_attributes_for :choices
 
     validates :content, :grid_x, :grid_y, presence: true
-    validates :grid_x, uniqueness: { scope: :grid_y, message: "Grid coordinates must be unique" }
+    validates :grid_x, uniqueness: { scope: :grid_y, message: "Story node grid coordinates must be unique" }
     validate :cannot_have_both_next_node_id_and_choices
     validate :cannot_share_grid_coordinates_with_a_choice
     validates_inclusion_of :color, :in => %w(red orange yellow green blue purple), :allow_nil => true
@@ -19,8 +19,8 @@ class StoryNode < ApplicationRecord
 
     def cannot_share_grid_coordinates_with_a_choice
         if story.choices.any? { |choice| choice.grid_x == grid_x && choice.grid_y == grid_y}
-            errors.add(:grid_x, "Grid coordinates must be unique")
-            errors.add(:grid_y, "Grid coordinates must be unique")
+            errors.add(:grid_x, "Story node grid coordinates must be unique")
+            errors.add(:grid_y, "Story node grid coordinates must be unique")
         end
     end
 end
