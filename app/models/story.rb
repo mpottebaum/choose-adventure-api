a080cfc1489d6d260b397550fc72f009b652f972
mpottebaum <mpottebaum@gmail.com>
2021-02-18 15:28:27 -0600

    validate story node coordinates are not shared with choice coordinates

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,6 +1,7 @@
 class Story < ApplicationRecord
     belongs_to :author
     has_many :story_nodes
+    has_many :choices, through: :story_nodes
 
     validates :title, presence: true
 end
--- app/models/story_node.rb
+++ app/models/story_node.rb
@@ -6,6 +6,7 @@ class StoryNode < ApplicationRecord
     validates :content, :grid_x, :grid_y, presence: true
     validates :grid_x, uniqueness: { scope: :grid_y, message: "Grid coordinates must be unique" }
     validate :cannot_have_both_next_node_id_and_choices
+    validate :cannot_share_grid_coordinates_with_a_choice
     validates_inclusion_of :color, :in => %w(red orange yellow green blue purple), :allow_nil => true
 
     private
@@ -13,6 +14,13 @@ class StoryNode < ApplicationRecord
     def cannot_have_both_next_node_id_and_choices
         if next_node_id.present? && choices.length > 0
             errors.add(:next_node_id, "Node cannot have both a next_node_id and choices")
+        end
+    end
+
+    def cannot_share_grid_coordinates_with_a_choice
+        if story.choices.any? { |choice| choice.grid_x == grid_x && choice.grid_y == grid_y}
+            errors.add(:grid_x, "Grid coordinates must be unique")
+            errors.add(:grid_y, "Grid coordinates must be unique")
         end
     end
 end
